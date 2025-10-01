04e28ee030134bf8a10193f588e7e415b01d1334
Anandhu Udayan <anandhuudayan180@gmail.com>
2025-10-01 21:43:36 +0530

    feat: implement screen home UI

---
+++ lib/Screens/home/screen_home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:money_manager_app/Screens/category/screen_categotry.dart';
+import 'package:money_manager_app/Screens/home/widgets/bottom_navigation_bar.dart';
+import 'package:money_manager_app/Screens/transactions/screen_transactions.dart';
+
+class ScreenHome extends StatelessWidget {
+  ScreenHome({super.key});
+
+  static ValueNotifier<int> newIndexNotifier = ValueNotifier(0);
+  final pages = [ScreenTransactions(), ScreenCategotry()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Money Manager',
+          style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        onPressed: () {
+          if (newIndexNotifier.value == 0) {
+            print('ScreenTransactions');
+          } else {
+            print('ScreenCategory');
+          }
+        },
+        child: Icon(Icons.add, color: Colors.white),
+      ),
+      bottomNavigationBar: MoneyManagerBottomNavigation(),
+      body: ValueListenableBuilder(
+        valueListenable: newIndexNotifier,
+        builder: (BuildContext context, int newIndex, child) {
+          return pages[newIndex];
+        },
+      ),
+    );
+  }
+}
---
+++ lib/Screens/home/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:money_manager_app/Screens/home/screen_home.dart';
+
+class MoneyManagerBottomNavigation extends StatelessWidget {
+  const MoneyManagerBottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: ScreenHome.newIndexNotifier,
+      builder: (BuildContext context, int newIndex, child) {
+        return BottomNavigationBar(
+          selectedItemColor: Colors.purple,
+
+          currentIndex: newIndex,
+          onTap: (index) {
+            ScreenHome.newIndexNotifier.value = index;
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Transactions',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Category',
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
